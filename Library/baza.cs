a7fad2f76b8e8dc7fa44f94ac0f2753781ea4463
Jon Rojnik <jon.rojnik@scv.si>
2021-03-29 14:22:14 +0200

    baza

---
+++ Library/baza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Library
+{
+    class baza
+    {
+        public static SQLiteConnection connect()
+        {
+            SQLiteConnection con;
+            con = new SQLiteConnection("data source = librarybaza.db");
+            return con;
+
+        }
+    }
+}
